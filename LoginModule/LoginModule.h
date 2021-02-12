288581b5e36f4ab1ef286d04906dadc9f453edc0
Vaibhav Bangde <vaibhav.bangde@hilti.com>
2021-02-12 13:04:26 +0530

    Initial Commit

---
+++ LoginModule/LoginModule.h
@@ -0,0 +1,18 @@
+//
+//  LoginModule.h
+//  LoginModule
+//
+//  Created by Vaibhav Bangde on 12/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoginModule.
+FOUNDATION_EXPORT double LoginModuleVersionNumber;
+
+//! Project version string for LoginModule.
+FOUNDATION_EXPORT const unsigned char LoginModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoginModule/PublicHeader.h>
+
+
